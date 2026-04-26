56593cebd51ecb3ddb60f4a843d0c1a366d72119
Patryk Jozwiak <patryk.jozwiak2001@gmail.com>
2026-04-26 11:57:50 +0200

    APIKEY fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,8 @@ import os
 st.set_page_config(layout="wide", page_title="Gemini chatbot app")
 st.title("Gemini chatbot app")
 
-# api_key, base_url = os.environ["API_KEY"], os.environ["BASE_URL"]
-api_key, base_url = ...
-selected_model = ...
+api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
+selected_model = "gemini-2.5-flash"
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
