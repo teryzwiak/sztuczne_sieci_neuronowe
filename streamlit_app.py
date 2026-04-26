3799792846bedc47a7bdf570cc937b487ffc7b87
Patryk Jozwiak <patryk.jozwiak2001@gmail.com>
2026-04-26 12:03:59 +0200

    Client and response implementation

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,13 @@ if prompt := st.chat_input():
     if not api_key:
         st.info("Invalid API key.")
         st.stop()
-    client = ...
+    client = OpenAI(api_key=api_key, base_url=base_url)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = ...
+    response = client.chat.completions.create(
+        model=selected_model,
+        messages=st.session_state.messages
+    )
 
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
