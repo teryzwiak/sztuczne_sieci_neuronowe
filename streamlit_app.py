055c81409439b6fc5ee56ccfcee16104451c7261
Patryk Jozwiak <patryk.jozwiak2001@gmail.com>
2026-04-26 11:43:23 +0200

    fast fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.set_page_config(layout="wide", page_title="Gemini chatbot app")
 st.title("Gemini chatbot app")
 
 # api_key, base_url = os.environ["API_KEY"], os.environ["BASE_URL"]
-api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
-selected_model = "gemini-2.5-flash"
+api_key, base_url = ...
+selected_model = ...
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
@@ -19,13 +19,11 @@ if prompt := st.chat_input():
     if not api_key:
         st.info("Invalid API key.")
         st.stop()
-    client = OpenAI(api_key=api_key, base_url=base_url)
+    client = ...
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(
-        model=selected_model,
-        messages=st.session_state.messages
-    )
+    response = ...
+
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
