8d3835ad17f5cbcec5fc1c7a1cbefb90bbfd1a3b
Patryk Jozwiak <patryk.jozwiak2001@gmail.com>
2026-04-26 13:56:19 +0200

    pdf fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,11 +18,9 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-uploaded_file = st.file_uploader("Upload an file", type=["pdf"])
-
 def load_pdf(file_path):
-    doc = fitz.open(file_path)
-    text = ""
+    doc=fitz.open(file_path)
+    text=""
     for page in doc:
         text += page.get_text()
     doc.close()
@@ -32,11 +30,20 @@ def load_documents_from_folder(folder_path):
     documents = []
     for filename in os.listdir(folder_path):
         if filename.endswith(".pdf"):
-            file_path = os.path.join(folder_path, filename)
-            text = load_pdf(file_path)
-            documents.append({"filename": filename, "content": text})
-    return documents
+            text = load_pdf(os.path.join(folder_path, filename))
+            documents.append({"filename": filename, "text": text})
+        return documents
 
+with st.sidebar:
+    uploaded_file = st.file_uploader("Choose a file", type="pdf")
+    if uploaded_file is not None:
+        doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+        file_content = ""
+        for page in doc:
+            file_content += page.get_text()
+        doc.close()
+        st.success(f"Wczytano: {uploaded_file.name}")
+        st.success(f"Tekst: {file_content}")
 
 
 if prompt := st.chat_input():
